2522b434477bed9ea1d382d96fa0e30cb35db2e6
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2024-10-11 08:50:08 +0200

    Move Components Classes

--- Library/Target/chrome/Engine.js
+++ Library/Target/chrome/Engine.js
@@ -1,4 +1,5 @@
 import Events from '../../Events.js';
+import Toolbar from './Components/Toolbar.js';
 
 export default (new class Engine {
     on(core, name, callback) {
@@ -13,62 +14,7 @@ export default (new class Engine {
 
     createToolbar(core) {
         return new Promise((success, failure) => {
-            success(new class Toolbar {
-                Callbacks = null;
-                Text = null;
-                Content = null;
-                Icon = null;
-
-                constructor() {
-                    this.Callbacks = new Events();
-
-                    chrome.action.onClicked.addListener((event) => {
-                        this.Callbacks.emit('onClick', event);
-                    });
-
-                    chrome.action.enable();
-                    this.update();
-                }
-
-                update() {
-                    if (this.Icon !== null) {
-                        chrome.action.setIcon({
-                            path: this.Icon
-                        });
-                    }
-
-                    if (this.Text !== null) {
-                        chrome.action.setTitle({
-                            title: this.Text
-                        });
-                    }
-
-                    if (this.Content !== null) {
-                        chrome.action.setPopup({
-                            popup: this.Content
-                        });
-                    }
-                }
-
-                setText(text) {
-                    this.Text = text;
-                    this.update();
-                }
-
-                setIcon(file) {
-                    this.Icon = file;
-                    this.update();
-                }
-
-                setContent(file) {
-                    this.Content = file;
-                    this.update();
-                }
-
-                onClick(callback) {
-                    this.Callbacks.on('onClick', callback);
-                }
-            });
+            success(new Toolbar());
         });
     }
 }());
--- Library/Target/mozilla/Engine.js
+++ Library/Target/mozilla/Engine.js
@@ -1,4 +1,5 @@
 import Events from '../../Events.js';
+import Toolbar from './Components/Toolbar.js';
 
 export default (new class Engine {
     on(core, name, callback) {
@@ -13,64 +14,7 @@ export default (new class Engine {
 
     createToolbar(core) {
         return new Promise((success, failure) => {
-            success(new class Toolbar {
-                Callbacks = null;
-                Text = null;
-                Content = null;
-                Icon = null;
-
-                constructor() {
-                    this.Callbacks = new Events();
-
-                    browser.browserAction.onClicked.addListener((event) => {
-                        this.Callbacks.emit('onClick', event);
-                    });
-
-                    browser.tabs.onActivated.addListener((query) => {
-                        browser.browserAction.enable();
-                        this.update();
-                    });
-                }
-
-                update() {
-                    if (this.Icon !== null) {
-                        browser.browserAction.setIcon({
-                            path: this.Icon
-                        });
-                    }
-
-                    if (this.Text !== null) {
-                        browser.browserAction.setTitle({
-                            title: this.Text
-                        });
-                    }
-
-                    if (this.Content !== null) {
-                        browser.browserAction.setPopup({
-                            popup: this.Content
-                        });
-                    }
-                }
-
-                setText(text) {
-                    this.Text = text;
-                    this.update();
-                }
-
-                setIcon(file) {
-                    this.Icon = file;
-                    this.update();
-                }
-
-                setContent(file) {
-                    this.Content = file;
-                    this.update();
-                }
-
-                onClick(callback) {
-                    this.Callbacks.on('onClick', callback);
-                }
-            });
+            success(new Toolbar);
         });
     }
 }());
